4f46b33623b29dd5a34429e83f399ec542f3dabd
Stephen Bennett <spb@exherbo.org>
2009-04-22 18:52:41 +0100

    Fix module symbol access

--- syn.h
+++ syn.h
@@ -16,11 +16,13 @@ inline void use_syn_main_symbols(module_t *m)
     MODULE_USE_SYMBOL(syn_conftable, "syn/main", "syn_conftable");
     MODULE_USE_SYMBOL(syn_report, "syn/main", "syn_report");
     MODULE_USE_SYMBOL(syn_debug, "syn/main", "syn_debug");
-    MODULE_USE_SYMBOL(syn, "syn/main", "syn");
+    service_t **syn_tmp;
+    MODULE_USE_SYMBOL(syn_tmp, "syn/main", "syn");
+    syn = *syn_tmp;
 }
 
 kline_t* (*syn_find_kline)(const char *, const char *);
-void (*syn_kline)(const char *, const char *);
+void (*syn_kline)(const char *, int, const char *);
 
 inline void use_syn_kline_symbols(module_t *m)
 {
